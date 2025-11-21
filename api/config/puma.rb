1cf82e33f8eca72cc474efb1d9443d3175561c19
karekauan <costakauanantonye@gmail.com>
2025-11-21 11:27:33 -0300

    mudando server para 0.0.0.0 para aceitar requests do mobile

--- api/config/puma.rb
+++ api/config/puma.rb
@@ -29,7 +29,10 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Bind to all interfaces (0.0.0.0) to allow connections from mobile devices on the network
+# In development, this allows the mobile app to connect from other devices
+port_number = ENV.fetch("PORT", 3000)
+bind "tcp://0.0.0.0:#{port_number}"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
